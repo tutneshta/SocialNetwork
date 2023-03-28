b414f3c84ef41b1a2d47b5bb107b85b19f29a459
tutneshta <tutneshta@yandex.ru>
2023-03-28 13:17:06 +0300

    Добавьте файлы проекта.

---
+++ AwesomeNetwork/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using AwesomeNetwork.Models;
+using AwesomeNetwork.Data.UoW;
+using AutoMapper;
+using AwesomeNetwork.Models.Users;
+using AwesomeNetwork.ViewModels.Account;
+using Microsoft.AspNetCore.Identity;
+
+namespace AwesomeNetwork.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly SignInManager<User> _signInManager;
+
+        public HomeController(ILogger<HomeController> logger, SignInManager<User> signInManager)
+        {
+            _logger = logger;
+            _signInManager = signInManager;
+        }
+
+        [Route("")]
+        [Route("[controller]/[action]")]
+        public IActionResult Index()
+        {
+            if (_signInManager.IsSignedIn(User))
+            {
+                return RedirectToAction("MyPage", "AccountManager");
+            }
+            else
+            {
+                return View(new MainViewModel());
+            }
+        }
+
+        [Route("[action]")]
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ AwesomeNetwork/Models/Users/User.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AwesomeNetwork.Models.Users
+{
+    public class User : IdentityUser
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string MiddleName { get; set; }
+
+        public DateTime BirthDate { get; set; }
+
+        public string Image { get; set; }
+
+        public string Status { get; set; }
+
+
+        public string About { get; set; }
+
+        public string GetFullName()
+        {
+            return FirstName + " " + MiddleName + " " + LastName;
+        }
+
+        public User()
+        {
+            Image = "http://pngimg.com/uploads/husky/husky_PNG19.png";
+            Status = "Ура! Я в соцсети!";
+            About = "Информация обо мне.";
+        }
+    }
+}
